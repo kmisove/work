7038988c741be3f103e30b5f09322f3f105ae948
kmisove <kmisove@netspace.sk>
2013-07-22 14:20:18 +0200

    Users.rb tu by sa mala nachadzat chyba

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,28 +1,37 @@
 class User < ActiveRecord::Base
   require 'digest/md5'
-  attr_accessible :email_string, :first_name, :last_name, :password, :password_confirmation
+  attr_accessible :email_string, :first_name, :last_name, :password, :password_confirmation,
+  
   before_save :encrypt_password
   
-  validates :first_name,
-  :presence => TRUE,
-  :length => {
-    :minimum => 2,
-    :allow_blank => TRUE
-  }
   
-  validates :last_name,
-  :presence => TRUE
-  :length => {
-    :minimum => 2,
-    :allow_blank => TRUE
-  }
   
-  validates :password,
+  validates_length_of :first_name,
   :presence => TRUE,
-  :length = {
-    :minumum => 6 
-    
-  },
+  :minimum   => 3,
+  :maximum   => 12,
+  :too_short => "must have at least {{count}} words",
+  :too_long  => "must have at most {{count}} words",
+  :allow_blank => TRUE 
+  
+  
+  validates_length_of :last_name, 
+  :presence => TRUE,
+  :minimum   => 3,
+  :maximum   => 12,
+  :too_short => "must have at least {{count}} words",
+  :too_long  => "must have at most {{count}} words",
+  :allow_blank => TRUE 
+  
+  
+  validates_length_of :password,
+  :presence => TRUE,
+  :minimum   => 6,
+  :maximum => 32,
+  :too_short => "must have at least {{count}} words",
+  :too_long  => "must have at most {{count}} words",
+  :allow_blank => TRUE,
+  
   :confirmation => TRUE
   validates :password_confirmation,
   :presence => TRUE
